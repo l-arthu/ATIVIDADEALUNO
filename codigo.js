a908fb9cb83d7acb1db2a9ad41ef314eb595e948
l-arthu <arthur05luis@gmail.com>
2023-05-25 10:29:56 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,16 @@
+function Aluno(){
+    let nome = window.prompt('Digite sue Nome:')
+    let n1 = Number(window.prompt('Digite a primeira nota')) 
+    let n2 = Number(window.prompt('Digite a segunda nota'))
+    let n3 = Number(window.prompt('Digite a terceira nota'))
+    media = (n1+n2+n3)/3
+    
+    if (media >= 6 ){
+    document.querySelector(".aluno").innerHTML="<br><p style= color:green >"+"O aluno "+nome+" ficou com média de "+media+", logo foi aprovado!"+"</p>"}
+    
+    else {
+   document.querySelector(".aluno").innerHTML="<br><p style= color:red >"+"O aluno "+nome+" ficou com média de "+media+", logo foi reprovado!"+"</p>"}
+
+}
+
+
